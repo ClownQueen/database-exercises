4974d269ab7b3dec61cad605254020a929336cd4
Micah Wood <micahswood7@gmail.com>
2022-03-28 14:43:51 -0500

    finished select_exercises.sql

---
+++ select_exercises.sql
@@ -0,0 +1,11 @@
+SELECT * FROM albums Where artist = 'Pink Floyd'
+
+SELECT * FROM albums Where release_date = 1967
+
+SELECT * FROM albums Where genre = 'Grunge, alternative rock'
+
+SELECT * FROM albums Where release_date = 199%
+
+SELECT * FROM albums Where sales = 20
+
+SELECT * FROM albums Where genre = 'rock'
