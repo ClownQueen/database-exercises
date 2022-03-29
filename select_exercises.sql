52ab1259a11976669da81756b75648f16e5ba100
Micah Wood <micahswood7@gmail.com>
2022-03-29 09:36:58 -0500

    finished and edited my sql exercises with help

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,11 +1,17 @@
-SELECT * FROM albums Where artist = 'Pink Floyd'
+SELECT album_name
+FROM albums
+Where artist = 'Pink Floyd'
 
-SELECT * FROM albums Where release_date = 1967
+SELECT release_date
+FROM albums
+Where album_name = 'Sgt. Pepper''s lonely hearts club band'
 
-SELECT * FROM albums Where genre = 'Grunge, alternative rock'
+SELECT album_name
+FROM albums
+Where genre = 'Grunge, alternative rock'
 
-SELECT * FROM albums Where release_date = 199%
+SELECT * FROM albums Where release_date = between 1990 and 1999
 
-SELECT * FROM albums Where sales = 20
+SELECT * FROM albums Where sales < 20
 
 SELECT * FROM albums Where genre = 'rock'
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,13 +1,15 @@
-SELECT album_name
-FROM albums;
+SELECT * FROM albums;
 
-SELECT release_date
-FROM albums;
+SELECT release_date FROM albums WHERE release_date < 1980;
 
 SELECT * FROM albums WHERE artist = 'Michael Jackson'
 
 UPDATE albums
 SET sales = sales * 10
+
+UPDATE albums
+SET release_date = 1800
+WHERE release_date < 1980
 
 UPDATE albums
 SET artist = 'Peter Jackson'
